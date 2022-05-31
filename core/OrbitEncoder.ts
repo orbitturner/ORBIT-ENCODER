18388548479f9dcfed1d5febe5848905497109d9
Orbit Turner <orbitturner@gmail.com>
2022-05-31 18:05:12 +0000

    🟢 RELEASING V 2.1.0 ✨

--- core/OrbitEncoder.ts
+++ core/OrbitEncoder.ts
@@ -19,6 +19,9 @@ global.Buffer = global.Buffer || Buffer;
  */
 
 export class OrbitEncoder {
+  // =======================================================================
+  // ## ENCODERS                                                          ##
+  // =======================================================================
   /**
    * ENCODING & COMPRESSING OBJECT TO UTF16 STRING.
    *
@@ -50,6 +53,41 @@ export class OrbitEncoder {
   }
 
   /**
+   * ENCODING & COMPRESSING OBJECT Using The Non-Standard Compress of LZ_STRING.
+   *
+   * Produces invalid UTF-16 strings.
+   * Those can be stored in localStorage only on webkit browsers
+   * (Tested on Android, Chrome, Safari).
+   *
+   * USE IT CAREFULLY !
+   */
+  public static rawEncode(obj: any): any {
+    if (typeof obj !== 'object' && !Array.isArray(obj)) {
+      return LZString.compress(obj);
+    }
+    return LZString.compress(JSON.stringify(obj));
+  }
+
+  /**
+   * ENCODING & COMPRESSING OBJECT Using Directly CompressUTF16 of LZ_STRING.
+   *
+   * Produces "valid" UTF-16 strings in the sense that all browsers can store them safely.
+   * So they can be stored in localStorage on all browsers
+   * Tested (IE9-10, Firefox, Android, Chrome, Safari).
+   *
+   * USE IT CAREFULLY !
+   */
+  public static rawEncodeUTF16(obj: any): any {
+    if (typeof obj !== 'object' && !Array.isArray(obj)) {
+      return LZString.compressToUTF16(obj);
+    }
+    return LZString.compressToUTF16(JSON.stringify(obj));
+  }
+
+  // =======================================================================
+  // ## DECODERS                                                          ##
+  // =======================================================================
+  /**
    * DECODING STRING CREATED WITH 'encode' Method.
    *
    * Since everything is an object in Javascript this will
@@ -75,6 +113,32 @@ export class OrbitEncoder {
       return JSON.parse(Buffer.from(LZString.decompressFromEncodedURIComponent(obj), 'base64').toString('binary'));
     } catch {
       return Buffer.from(LZString.decompressFromEncodedURIComponent(obj), 'base64').toString('binary');
+    }
+  }
+
+  /**
+   * DECODING STRING CREATED WITH 'rawEncode' Method.
+   *
+   * @see: https://developer.mozilla.org/en-US/docs/Glossary/Base64
+   */
+  public static rawDecode(obj: any): any {
+    try {
+      return JSON.parse(LZString.decompress(obj));
+    } catch {
+      return LZString.decompress(obj);
+    }
+  }
+
+  /**
+   * DECODING STRING CREATED WITH 'rawEncodeUTF16' Method.
+   *
+   * @see: https://developer.mozilla.org/en-US/docs/Glossary/Base64
+   */
+  public static rawDecodeUTF16(obj: any): any {
+    try {
+      return JSON.parse(LZString.decompressFromUTF16(obj));
+    } catch {
+      return LZString.decompressFromUTF16(obj);
     }
   }
 }
--- core/__test__/OrbitEncoder.test.ts
+++ core/__test__/OrbitEncoder.test.ts
@@ -10,6 +10,16 @@ const User = {
   },
 };
 
+test('Trying to Encode Some Data with Raw Encode Method', () => {
+  const result = OrbitEncoder.rawEncode(User);
+  expect(typeof result).toBe('string');
+});
+
+test('Trying to Encode Some Data with Raw Encode UTF-16 Method', () => {
+  const result = OrbitEncoder.rawEncodeUTF16(User);
+  expect(typeof result).toBe('string');
+});
+
 test('Trying to Encode Some Data with Encode Method', () => {
   const result = OrbitEncoder.encode(User);
   expect(typeof result).toBe('string');
@@ -24,4 +34,22 @@ test('Decoding With URI SAFE', () => {
   const result = OrbitEncoder.encodeWithURIsafe(User);
 
   expect(OrbitEncoder.decodeURIsafe(result)).toEqual(User);
+});
+
+test('Decoding With RawUTF16-EncLZ', () => {
+  const result = OrbitEncoder.rawEncodeUTF16(User);
+
+  expect(OrbitEncoder.rawDecodeUTF16(result)).toEqual(User);
+});
+
+test('Decoding With Raw-EncLZ', () => {
+  const result = OrbitEncoder.rawEncode(User);
+
+  expect(OrbitEncoder.rawDecode(result)).toEqual(User);
+});
+
+test('Decoding With Standards-Decode', () => {
+  const result = OrbitEncoder.encode(User);
+
+  expect(OrbitEncoder.decode(result)).toEqual(User);
 });
